873776109fb3a8834ad8c1acc15ebff49b963f7c
HugoCidralDasChagas <hugochagasuk@gmail.com>
2023-04-20 12:25:29 +0100

    add task 1 code

--- BankAccount/src/banking/BankAccount.java
+++ BankAccount/src/banking/BankAccount.java
@@ -2,5 +2,62 @@ package banking;
 
 public class BankAccount {
 	
+	//properties
+	private String name;
+	private int accountNumber;
+	private double balance;
+	
+	//class variable
+	static int id = 100_000;
+	
+	//constructor
+	public BankAccount(String name, double balance) {
+		this.name = name;
+		this.balance = balance;
+		this.accountNumber = ++id;
+	}
+	
+	//getters
+	public String getName() {
+		return this.name;
+	}
+	
+	public int accountNumber() {
+		return this.accountNumber;
+	}
+	
+	public double balance() {
+		return this.balance;
+	}
+	
+	
+	//setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//no need to set accountNumber (automatic generated)
+	//no need to set balance (overwrite current balance)
+	
+	
+	//methods
+	public void deposit(double amount) {
+		this.balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		if (amount <= 0) {
+			System.out.println("You don't have enough money to withdraw");
+		} else {
+			this.balance -= amount;
+		}
+	}
+	
+
+	
+	
+	
+	
+	
 
 }
