c6939dd969930e2061c2f06f6b75861d658c92a0
HugoCidralDasChagas <hugochagasuk@gmail.com>
2023-04-20 11:38:29 +0100

    second commit

---
+++ BankAccount/src/bank/BankAccount.java
@@ -0,0 +1,18 @@
+package bank;
+
+public class BankAccount {
+	
+	public BankAccount (String customerName, double balance) {
+		customerName = "Jake";
+		balance = 0;
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
